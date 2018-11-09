77889081ba0356969d361ca5d82f4978eb2ae6bd
bboampong <baffouraduboampong@gmail.com>
2018-11-09 11:32:21 +0000

    Updated Home Page with list demo applications

---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 mt-5 text-center">
+            <img src="https://epaygh.com/images/logo2.png" alt="Epay Logo" width="200">
+            <h1>Demo Applications</h1>
+            <p>Please select a sample application to try it out</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 offset-lg-2 col-md-6 col-12 mt-5">
+            <div class="card">
+                <h5 class="card-header">Demo Application</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Online Bookshop</h5>
+                    <p class="card-text">An example application which seeks to show how an online bookshop can integrate Epay.</p>
+                    <a href="#" class="btn btn-primary">Try Demo App</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-12 mt-5">
+            <div class="card">
+                <h5 class="card-header">Demo Application</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Event Registration</h5>
+                    <p class="card-text">An example application which seeks to show how businesses can use Epay for Events.</p>
+                    <a href="#" class="btn btn-primary">Try Demo App</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 offset-lg-2 col-md-6 col-12 mt-5 mb-5">
+            <div class="card">
+                <h5 class="card-header">Demo Application</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Sponsorship Purchase</h5>
+                    <p class="card-text">An exmaple application demonstrating how a business can use Epay to take subscriptions.</p>
+                    <a href="#" class="btn btn-primary">Try Demo App</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-12 mt-5 mb-5">
+            <div class="card">
+                <h5 class="card-header">Featured Production Application</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Online Bookshop</h5>
+                    <p class="card-text">An example application currently in production by a client who sells marraige books online.</p>
+                    <a href="http://mkshop.cdsportals.com/" class="btn btn-primary">Try App</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Epay Example Apps</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
